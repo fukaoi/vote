138149f07c7c8701bb762cd69af084b49a71b33b
fukaoi <goodakira7@gmail.com>
2021-02-27 16:40:18 +0900

    updated

---
+++ src/programs/src/lib.rs
@@ -0,0 +1,27 @@
+use byteorder::{ByteOrder, LittleEndian};
+
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+};
+
+use std::mem;
+
+entrypoint!(process_instruction);
+
+fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    msg!("Rust program entrypoint");
+
+    let accounts_iter = &mut accounts.iter();
+    let account = next_account_info(accounts_iter)?;
+    msg!("{:?}", account); 
+    Ok(())
+}
